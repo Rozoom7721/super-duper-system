a1fd94573c899a9de0f491aafcb8241bae78e463
Mateusz <101966939+Rozoom7721@users.noreply.github.com>
2023-11-02 18:45:19 +0100

    Some change

--- MakeAMealApp/Program.cs
+++ MakeAMealApp/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
+            Meals meals = new Meals();
+            List<Meals> mealList = new List<Meals>();
+            
             Console.WriteLine("Hello, World!");
+        }
+        class Meals
+        {
+            public string mealName;
+            public string mealDescription;
+            public string mealRecipe;
+            public int mealCal;
+
+            public void Initialize(string name, string description, string recipe, int cal)
+            {
+                mealName = name;
+                mealDescription = description;
+                mealRecipe = recipe;
+                mealCal = cal;
+            }
         }
     }
 }
